c5755a0e672fefc7c71bdc67e546ee51b38b81f3
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-04-22 22:25:48 +0200

    zdc: dodaj klasę błędu

---
+++ zdc/src/zd/error.cpp
@@ -0,0 +1,38 @@
+#include <zd/error.hpp>
+
+using namespace zd;
+
+error::~error()
+{
+    if (_argv)
+    {
+        delete[] _argv;
+    }
+}
+
+error &
+error::operator=(error &&that) noexcept
+{
+    if (this == &that)
+    {
+        return *this;
+    }
+
+    this->~error();
+    _origin = that._origin;
+    _ordinal = that._ordinal;
+    _argc = std::exchange(that._argc, 0);
+    _argv = std::exchange(that._argv, nullptr);
+    return *this;
+}
+
+const uintptr_t
+error::operator[](size_t i) const
+{
+    if (_argc <= i)
+    {
+        return 0;
+    }
+
+    return _argv[i];
+}
