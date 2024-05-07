64921e8036d25c1d1dff48870245e2ca030b2fde
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-05-08 00:10:48 +0200

    zdc: buduj tabelę komunikatów pod Windows

--- zdc/src/messages.cpp
+++ zdc/src/messages.cpp
@@ -7,6 +7,10 @@
 #include <zd/lex/pl_istream.hpp>
 #include <zd/par/parser.hpp>
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wformat-security"
 #endif
@@ -21,6 +25,7 @@ make_id(Torig origin, Tord ordinal)
            static_cast<uint16_t>(ordinal);
 }
 
+#ifndef _WIN32
 const struct _msg
 {
     uint16_t    id;
@@ -83,6 +88,7 @@ retrieve_fmt(uint8_t origin, uint8_t ordinal)
     case num:                                                                  \
         std::fprintf(stderr, (fmt), ARGV##num(argv));                          \
         break;
+#endif
 
 void
 print_error(const error &err)
@@ -90,6 +96,22 @@ print_error(const error &err)
     auto path = err.file().empty() ? "input" : err.file().data();
     std::fprintf(stderr, "%s:%u:%u: error: ", path, err.line(), err.column());
 
+#ifdef _WIN32
+    auto args =
+        reinterpret_cast<DWORD_PTR *>(const_cast<uintptr_t *>(err.begin()));
+    LPWSTR buffer{nullptr};
+    DWORD  length = ::FormatMessageW(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_ARGUMENT_ARRAY |
+            FORMAT_MESSAGE_FROM_HMODULE,
+        nullptr, make_id(err.origin(), err.ordinal()),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        reinterpret_cast<LPWSTR>(&buffer), 0,
+        reinterpret_cast<va_list *>(args));
+    ::WriteConsoleW(::GetStdHandle(STD_ERROR_HANDLE), buffer, length, NULL,
+                    NULL);
+    ::LocalFree(buffer);
+
+#else
     if (auto fmt = retrieve_fmt(err.origin(), err.ordinal()))
     {
         switch (err.size())
@@ -99,7 +121,8 @@ print_error(const error &err)
         default:
             std::fputs(fmt, stderr);
         }
-
-        std::fputs("\n", stderr);
     }
+#endif
+
+    std::fputs("\n", stderr);
 }
---
+++ zdc/src/zdc.mc
@@ -0,0 +1,77 @@
+﻿LanguageNames=(English=0x0409:zdc_en_US)
+
+; // Stream errors
+
+MessageId=0x0100
+Language=English
+unexpected byte %1!u! in the encoding '%2!S!'
+.
+
+MessageId=0x0101
+Language=English
+invalid multi-byte sequence
+.
+
+MessageId=0x0102
+Language=English
+read error in a multi-byte sequence
+.
+
+; // Lexer errors
+
+MessageId=0x0200
+Language=English
+invalid line break
+.
+
+MessageId=0x0201
+Language=English
+unexpected character %1!d! in '%2!S!'
+.
+
+; // Parser errors
+
+MessageId=0x0300
+Language=English
+end of file
+.
+
+MessageId=0x0301
+Language=English
+unexpected '%1!S!' in '%2!S!'
+.
+
+MessageId=0x0302
+Language=English
+unexpected end of file while processing '%1!S!'
+.
+
+MessageId=0x0303
+Language=English
+unknown directive
+.
+
+MessageId=0x0304
+Language=English
+number %1!d! is out of range
+.
+
+MessageId=0x0305
+Language=English
+name expected after '%1!S!', got '%2!S!'
+.
+
+MessageId=0x0306
+Language=English
+include path expected
+.
+
+MessageId=0x0307
+Language=English
+unexpected '%1!S!' at the beginning of a command
+.
+
+MessageId=0x0308
+Language=English
+unexpected character %1!u! in the emit directive
+.
