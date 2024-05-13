a0e2e9964cdeeaa5cdcd78df5a90746c109de827
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-05-13 17:58:39 +0200

    gen: obsłuż deklaracje zmiennych w ZD4

--- zdc/include/zd/gen/zd4_generator.hpp
+++ zdc/include/zd/gen/zd4_generator.hpp
@@ -13,12 +13,42 @@ namespace zd
 namespace gen
 {
 
+enum class symbol_type
+{
+    undefined,
+    var_text,
+};
+
+struct symbol
+{
+    ustring           name;
+    symbol_type       type;
+    zd4_known_section section;
+    unsigned          offset;
+
+    symbol(ustring          &name_,
+           symbol_type       type_,
+           zd4_known_section section_,
+           unsigned          offset_)
+        : name{name_}, type{type_}, section{section_}, offset{offset_}
+    {
+    }
+};
+
 class zd4_generator : public generator
 {
     zd4_section _code;
     zd4_section _data;
+    zd4_section _udat;
+
+    std::list<symbol> _symbols;
+    unsigned          _symbol_num;
 
   public:
+    zd4_generator() : _code{}, _data{}, _udat{false}, _symbols{}, _symbol_num{0}
+    {
+    }
+
     bool
     process(const par::assignment_node &node) override
     {
@@ -35,10 +65,7 @@ class zd4_generator : public generator
     }
 
     bool
-    process(const par::declaration_node &node) override
-    {
-        return false;
-    }
+    process(const par::declaration_node &node) override;
 
     bool
     process(const par::end_node &node) override;
@@ -113,6 +140,18 @@ class zd4_generator : public generator
     link(std::FILE *output);
 
   private:
+    static ustring
+    get_cname(const ustring &name);
+
+    symbol &
+    get_symbol(const ustring &name);
+
+    bool
+    set_symbol(const ustring    &name,
+               symbol_type       type,
+               zd4_known_section section,
+               unsigned          offset);
+
     bool
     asm_mov(par::cpu_register dst, const ustring &src);
 
--- zdc/src/zd/gen/zd4_generator.cpp
+++ zdc/src/zd/gen/zd4_generator.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <cstdio>
+#include <cstring>
 
 #include <zd/gen/zd4_generator.hpp>
 #include <zd/par/node.hpp>
@@ -44,6 +46,30 @@ zd4_generator::process(const par::call_node &node)
 }
 
 bool
+zd4_generator::process(const par::declaration_node &node)
+{
+    if (!node.target->is<object_node>())
+    {
+        return false;
+    }
+
+    auto target = node.target->as<object_node>();
+    switch (target->type)
+    {
+    case object_type::text: {
+        return set_symbol(target->name, symbol_type::var_text,
+                          zd4_known_section::zd4_section_udat,
+                          _udat.reserve(UINT8_MAX));
+    }
+
+    default:
+        return false;
+    }
+
+    return false;
+}
+
+bool
 zd4_generator::process(const par::end_node &node)
 {
     if (node.name.empty())
@@ -66,6 +92,56 @@ zd4_generator::link(std::FILE *output)
 
     _code.relocate(output, bases);
     _data.relocate(output, bases);
+}
+
+ustring
+zd4_generator::get_cname(const ustring &name)
+{
+    ustring cname{};
+    std::for_each(name.begin(), name.end(), [&cname](int codepoint) {
+        cname.append(std::toupper(text::pl_toascii(codepoint)));
+    });
+    return std::move(cname);
+}
+
+symbol &
+zd4_generator::get_symbol(const ustring &name)
+{
+    auto cname = std::move(get_cname(name));
+    auto it =
+        std::find_if(_symbols.begin(), _symbols.end(), [&cname](symbol &sym) {
+            return sym.name == cname;
+        });
+
+    if (it == _symbols.end())
+    {
+        // Add undefined
+        _symbols.emplace_back(cname, symbol_type::undefined,
+                              zd4_known_section::zd4_section_unkn,
+                              _symbol_num++);
+        it = std::prev(it);
+    }
+
+    return *it;
+}
+
+bool
+zd4_generator::set_symbol(const ustring    &name,
+                          symbol_type       type,
+                          zd4_known_section section,
+                          unsigned          offset)
+{
+    auto &symbol = get_symbol(name);
+    if (zd4_known_section::zd4_section_unkn != symbol.section)
+    {
+        // Already defined
+        return false;
+    }
+
+    symbol.type = type;
+    symbol.section = section;
+    symbol.offset = offset;
+    return true;
 }
 
 #define ASM_REQUIRE(expr)                                                      \
