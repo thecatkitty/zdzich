7ddc58d4461b87ff71cdc56463bffa739986c305
Mateusz Karcz <mateusz.karcz@interia.eu>
2024-07-02 18:34:52 +0200

    app: wczytuj plik konfiguracyjny ZD4

--- app/zd4/src/arguments.cpp
+++ app/zd4/src/arguments.cpp
@@ -1,20 +1,42 @@
 #include "zd4.h"
 
+static void
+_process_arg(const char *arg, zd4_arguments *args)
+{
+    if ('/' != arg[0])
+    {
+        args->input = arg;
+        return;
+    }
+
+    if ('I' == arg[1])
+    {
+        args->inc_dirs.push_back(arg + 2);
+        return;
+    }
+}
+
 zd4_arguments::zd4_arguments(int argc, char *argv[])
     : input{nullptr}, inc_dirs{}
 {
+    auto cfg = std::fopen("zd.cfg", "r");
+    if (cfg)
+    {
+        char buff[PATH_MAX];
+        while (std::fgets(buff, PATH_MAX, cfg))
+        {
+            auto len = std::strlen(buff);
+            if ('\n' == buff[len - 1])
+            {
+                buff[len - 1] = 0;
+            }
+            _process_arg(buff, this);
+        }
+        std::fclose(cfg);
+    }
+
     for (int i = 1; i < argc; i++)
     {
-        if ('/' != argv[i][0])
-        {
-            input = argv[i];
-            continue;
-        }
-
-        if ('I' == argv[i][1])
-        {
-            inc_dirs.push_back(argv[i] + 2);
-            continue;
-        }
+        _process_arg(argv[i], this);
     }
 }
